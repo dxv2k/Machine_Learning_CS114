6e8ba5b968720032ba5c0e1343a77e6de941397d
KnightV <9569076+dxv2k@users.noreply.github.com>
2020-04-24 01:17:43 +0700

    all done

--- batch_gradient.py
+++ batch_gradient.py
@@ -2,8 +2,23 @@ import numpy as np
 import pandas as pd  
 print('Import libraries successfully')
 
-df = pd.read_csv('demo_data.csv')
 
+# Setting parameter
+etaList = [0.01, 0.1, 1, 10] # learning rate
+n_iterations = 1000
+m = 100
+thetaList = [np.ones((2,1)), np.array([0,0]), np.array([-5,-5])]
 
+def batchGradient(n_iterations, eta, theta, X_b, y): 
+  for iteration in range(n_iterations):
+     gradients = 2/m * X_b.T.dot(X_b.dot(theta) - y)
+     theta = theta - eta * gradients
+  return theta
 
-# def batchGradient():
+theta_best_list = {} 
+for eta in etaList: 
+  for theta in thetaList: 
+    temp = batchGradient(n_iterations, eta, theta, X_b,y)
+    theta_best_list.append(temp)
+theta_best_list
+print(batchGradient(n_iterations,eta, np.array([0,0]), X_b, y))
